0bc38a4d52e347ac3d1164a853b1c484a1dc98e6
Meruem117 <1572677319@qq.com>
2022-03-21 22:29:44 +0800

    update

--- src/main/java/com/niit/share/WeappShareApplication.java
+++ src/main/java/com/niit/share/WeappShareApplication.java
@@ -3,8 +3,9 @@ package com.niit.share;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @MapperScan("com.niit.share.mapper")
 public class WeappShareApplication {
 
