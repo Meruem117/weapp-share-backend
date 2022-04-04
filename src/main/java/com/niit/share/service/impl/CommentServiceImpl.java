65596ee03aadee949ae366e134c8c4fdff176a8f
Meruem117 <1572677319@qq.com>
2022-04-04 16:02:39 +0800

    update comment

--- src/main/java/com/niit/share/service/impl/CommentServiceImpl.java
+++ src/main/java/com/niit/share/service/impl/CommentServiceImpl.java
@@ -1,17 +1,24 @@
 package com.niit.share.service.impl;
 
 import com.niit.share.entity.Comment;
+import com.niit.share.entity.User;
 import com.niit.share.mapper.CommentMapper;
+import com.niit.share.mapper.UserMapper;
 import com.niit.share.service.CommentService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class CommentServiceImpl implements CommentService {
     @Resource
     private CommentMapper commentMapper;
+
+    @Resource
+    private UserMapper userMapper;
 
     @Override
     public List<Comment> getAll() {
@@ -30,6 +37,13 @@ public class CommentServiceImpl implements CommentService {
 
     @Override
     public Integer addComment(Comment comment) {
+        User user = userMapper.getUserById(comment.getUserId());
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String datetime = df.format(new Date());
+        comment.setUserName(user.getName());
+        comment.setAvatar(user.getAvatar());
+        comment.setLikes(0);
+        comment.setTime(datetime);
         commentMapper.addComment(comment);
         return comment.getId();
     }
