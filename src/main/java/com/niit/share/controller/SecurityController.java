75f7fec1b76d54504bd1e61ce6654c3fa0b6bfdf
Meruem117 <1572677319@qq.com>
2022-07-07 22:15:48 +0800

    update security controller

--- src/main/java/com/niit/share/controller/SecurityController.java
+++ src/main/java/com/niit/share/controller/SecurityController.java
@@ -7,6 +7,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecurityController {
     @GetMapping("/hello")
     public String hello() {
-        return "Login successfully";
+        return "Hello";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/main")
+    public String main() {
+        return "main";
+    }
+
+    @GetMapping("/error")
+    public String error() {
+        return "error";
     }
 }
