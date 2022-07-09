d3e339472356a2ccb83315ec011f9dad6ac1bffe
Meruem117 <1572677319@qq.com>
2022-07-09 22:21:23 +0800

    add thymeleaf

--- src/main/java/com/niit/share/controller/SecurityController.java
+++ src/main/java/com/niit/share/controller/SecurityController.java
@@ -2,6 +2,7 @@ package com.niit.share.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class SecurityController {
@@ -11,8 +12,8 @@ public class SecurityController {
     }
 
     @GetMapping("/login")
-    public String login() {
-        return "login";
+    public ModelAndView login() {
+        return new ModelAndView("login");
     }
 
     @GetMapping("/home")
