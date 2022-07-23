a8657e591f41a5174a2efaf4c40bd1ea5e0bbd4e
Meruem117 <1572677319@qq.com>
2022-07-23 22:38:13 +0800

    update security config

--- src/main/java/com/niit/share/config/SecurityUserConfig.java
+++ src/main/java/com/niit/share/config/SecurityUserConfig.java
@@ -3,6 +3,7 @@ package com.niit.share.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,5 +24,17 @@ public class SecurityUserConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder password() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/security/login")
+                .defaultSuccessUrl("/security/home").permitAll()
+                .and().authorizeRequests()
+                .antMatchers("/", "/security/hello").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable();
     }
 }
