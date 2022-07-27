bc5dbff465145b1a5aac27f9e571b6a6d10e3247
Meruem117 <1572677319@qq.com>
2022-07-27 22:07:18 +0800

    update

--- src/main/java/com/niit/share/controller/SecurityController.java
+++ src/main/java/com/niit/share/controller/SecurityController.java
@@ -1,22 +1,32 @@
 package com.niit.share.controller;
 
+import com.niit.share.base.response.BaseResponse;
+import com.niit.share.utils.ResUtils;
+import com.niit.share.utils.SecurityUtils;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/security")
 public class SecurityController {
+    @Resource
+    AuthenticationManager authenticationManager;
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello Security";
     }
 
-    @GetMapping("/login")
-    public ModelAndView login() {
-        return new ModelAndView("login");
+    @PostMapping("/login")
+    public BaseResponse<User> login(@RequestBody Map<String, String> params) {
+        User user = SecurityUtils.login(params.get("username"), params.get("password"), authenticationManager);
+        return ResUtils.success(user);
     }
 
     @GetMapping("/home")
@@ -31,7 +41,7 @@ public class SecurityController {
 
     @GetMapping("/admin")
     @Secured("ADMIN")
-    public ModelAndView error() {
+    public ModelAndView admin() {
         return new ModelAndView("admin");
     }
 }
