79cacf39abdc4bf089e1590667f780f464785d1a
Meruem117 <1572677319@qq.com>
2022-07-27 22:14:53 +0800

    remove

--- src/main/java/com/niit/share/config/WebSecurityConfig.java
+++ src/main/java/com/niit/share/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.niit.share.config;
 
-import com.niit.share.service.impl.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,7 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
