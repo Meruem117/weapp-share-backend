bb7fd4c7014198af512744d4c706eac2674a89dc
Meruem117 <1572677319@qq.com>
2022-08-02 22:02:49 +0800

    update config

--- src/main/java/com/niit/share/config/SecurityConfig.java
+++ src/main/java/com/niit/share/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -26,11 +27,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/security/login").permitAll()
-                .anyRequest().authenticated()
-                .and().formLogin().permitAll().loginProcessingUrl("/security/login")
-                .and().csrf().disable();
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/security/login").anonymous()
+                .anyRequest().authenticated();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
