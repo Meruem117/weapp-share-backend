53c9802324837779b7d18079c10e625da60c7379
Meruem117 <1572677319@qq.com>
2023-06-17 20:26:16 +0800

    remove security

--- src/main/java/com/niit/share/service/TokenService.java
+++
@@ -1,9 +0,0 @@
-package com.niit.share.service;
-
-import org.springframework.security.core.Authentication;
-
-public interface TokenService {
-    String getToken(Authentication authentication);
-
-    Boolean verifyToken(String token);
-}
